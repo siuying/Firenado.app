b0dd2700547a3d8a310bcbbfea85c8e34d6bf879
Francis Chong <francis@ignition.hk>
2015-07-31 22:49:28 +0800

    revise build script

--- bin/download_dependency.sh
+++ bin/download_dependency.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# download the libvlc 2.2.1 static build and move it to build folder
+# download the libvlc 2.2.1 static build and move vendor
 wget "https://dl.dropboxusercontent.com/u/3810814/libvlc_2.2.1_mac.zip" \
-  && unzip libvlc_2.2.1_mac.zip \
-  && mv lib app/node_modules/wcjs-player/node_modules/wcjs-renderer/node_modules/webchimera.js/build/Release/
+  && mv libvlc_2.2.1_mac.zip vendor
