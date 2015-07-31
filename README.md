cb08909040198dc0e64656d5045ced203fa6dd34
Francis Chong <francis@ignition.hk>
2015-07-31 15:41:31 +0800

    rename app

---
+++ README.md
@@ -0,0 +1,14 @@
+# TorrentPlay
+
+Search and playback media on BitTorrent.
+
+Technically it could work on Linux/Mac/Windows, currently only OSX build is ready.
+
+## About
+
+Based on following softwares:
+
+- [peerflix](https://github.com/mafintosh/peerflix)
+- [Webchimera.js](https://github.com/RSATom/WebChimera.js)
+- [libvlc](http://www.videolan.org/vlc/libvlc.html)
+- [Electron](https://github.com/atom/electron)
--- bin/build_osx.sh
+++ bin/build_osx.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+APP_NAME=TorrentPlay
+
 # Cleanup
-rm -rf build/PiratePlay/osx
-mkdir -p build/PiratePlay/osx
+rm -rf build/$APP_NAME/osx
+mkdir -p build/$APP_NAME/osx
 
 # Rebuild
 ./node_modules/.bin/electron-rebuild
@@ -10,10 +12,10 @@ CURRENT_DIR=`pwd`
 cd ./node_modules/wcjs-player/node_modules/wcjs-renderer/node_modules/webchimera.js/ && bash build_electron.sh && cd $CURRENT_DIR
 
 # Copy app to destination
-cp -R node_modules/electron-prebuilt/dist/Electron.app build/PiratePlay/osx
-cp -R app build/PiratePlay/osx/Electron.app/Contents/Resources/
+cp -R node_modules/electron-prebuilt/dist/Electron.app build/$APP_NAME/osx/${APP_NAME}.app
+cp -R app build/$APP_NAME/osx/${APP_NAME}.app/Contents/Resources/
 
 # Copy modules
 unzip vendor/libvlc_2.2.1_mac.zip -d node_modules/wcjs-player/node_modules/wcjs-renderer/node_modules/webchimera.js/build/Release/
-cp -R node_modules build/PiratePlay/osx/Electron.app/Contents/Resources/app
-rm -rf build/PiratePlay/osx/Electron.app/Contents/Resources/app/node_modules/{electron-prebuilt,electron-rebuild}
+cp -R node_modules build/$APP_NAME/osx/${APP_NAME}.app/Contents/Resources/app
+rm -rf build/$APP_NAME/osx/${APP_NAME}.app/Contents/Resources/app/node_modules/{electron-prebuilt,electron-rebuild}
