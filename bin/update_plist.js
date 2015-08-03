dcf1e1974f1b221ad560785d279bbd09373cc9eb
Francis Chong <francis@ignition.hk>
2015-08-03 20:41:04 +0800

    fix bundle name

--- bin/update_plist.js
+++ bin/update_plist.js
@@ -10,6 +10,7 @@ var appId = "hk.ignition.Firenado"
 var plistFile = 'build/' + appName + '/osx/Firenado.app/Contents/Info.plist'
 
 var obj = plist.parse(fs.readFileSync(plistFile, 'utf8'));
+obj["CFBundleName"] = appName
 obj["CFBundleDisplayName"] = appName
 obj["CFBundleVersion"] = appVersion
 obj["CFBundleIdentifier"] = appId
